24898dcdba0e4aaff63604d0749acf556aa5fb55
Adviti Singh <adviti.s@gmail.com>
2025-05-13 14:08:56 +0200

    random state cluster

--- scripts/reduce_and_cluster.py
+++ scripts/reduce_and_cluster.py
@@ -4,8 +4,8 @@ from umap import UMAP
 import re
 
 
-def reduce_embeddings(embeddings, n_neighbors=5, min_dist=0.3, metric="cosine"):
-    reducer = UMAP(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
+def reduce_embeddings(embeddings, n_neighbors=5, min_dist=0.3, metric="cosine", random_state=42):
+    reducer = UMAP(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric, random_state=random_state)
     return reducer.fit_transform(embeddings)
 
 
